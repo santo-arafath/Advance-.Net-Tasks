6051f8646d168d019406b44bbff1b3cf4d60f6fa
santo-arafath <arafath.hossen.santo97193@gmail.com>
2023-11-09 20:35:15 +0600

    Assignment v3

--- Zero_Hunger/DTOs/RequestDTO.cs
+++ Zero_Hunger/DTOs/RequestDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -12,16 +13,41 @@ namespace Zero_Hunger.DTOs
 
 
         }
+
+        
         public int req_id { get; set; }
+
+        [Required(ErrorMessage = "*Provide Food type")]
         public string food_type { get; set; }
-        public string Email { get; set; }
+
+
+
+        [Required(ErrorMessage = "*Provide you email")]
+
+/*        [RegularExpression("^\\@gmail.com$", ErrorMessage = "email format no correct")]
+*/        public string Email { get; set; }
+
+
+
+        
         public string Phone { get; set; }
+
+        [Required(ErrorMessage = "*Provide Quantity")]
+       
         public string quantity { get; set; }
+
+        [Required(ErrorMessage = "*Provide maximum prevention time")]
         public string max_preservation_time { get; set; }
+
+        [Required(ErrorMessage = "*Provide location")]
         public string location { get; set; }
+
+        
         public string status { get; set; }
         public Nullable<int> rest_id { get; set; }
         public Nullable<int> assigned_employee_id { get; set; }
+
+        [Required(ErrorMessage = "*Provide details")]
         public string details_food { get; set; }
 
         public Nullable<System.DateTime> date_of_order { get; set; }
